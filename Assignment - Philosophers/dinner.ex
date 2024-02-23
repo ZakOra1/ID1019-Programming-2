7203609afdca0aa5c27eaa1e7502447b9882b8fa
ZakOra1 <32168076+ZakOra1@users.noreply.github.com>
2024-02-23 18:43:29 +0100

    Added backoff time and benchmarking

--- Assignment - Philosophers/dinner.ex
+++ Assignment - Philosophers/dinner.ex
@@ -1,29 +1,39 @@
 defmodule Dinner do
-  def start(), do: spawn(fn -> init() end)
-  def init() do
+  def bench(hunger) do
+    start_time = :erlang.monotonic_time()
+    Dinner.start(hunger, self())
+    receive do
+      :done ->
+        end_time = :erlang.monotonic_time()
+        time = :erlang.convert_time_unit(end_time - start_time, :native, :millisecond)
+        IO.puts "Time: #{time} for hunger #{hunger}"
+    end
+  end
+  def start(hunger, parent), do: spawn(fn -> init(hunger, parent) end)
+  def init(hunger, parent) do
     c1 = Chopstick.start()
     c2 = Chopstick.start()
     c3 = Chopstick.start()
     c4 = Chopstick.start()
     c5 = Chopstick.start()
     ctrl = self()
-    Philosopher.start(5, c1, c2, :arendt, ctrl, 5)
-    Philosopher.start(5, c2, c3, :hypatia, ctrl, 5)
-    Philosopher.start(5, c3, c4, :simone, ctrl, 5)
-    Philosopher.start(5, c4, c5, :elisabeth, ctrl, 5)
-    Philosopher.start(5, c5, c1, :ayn, ctrl, 5)
-    wait(5, [c1, c2, c3, c4, c5])
+    Philosopher.start(hunger, c1, c2, :arendt, ctrl, 5, 0)
+    Philosopher.start(hunger, c2, c3, :hypatia, ctrl, 5, 0)
+    Philosopher.start(hunger, c3, c4, :simone, ctrl, 5, 0)
+    Philosopher.start(hunger, c4, c5, :elisabeth, ctrl, 5, 0)
+    Philosopher.start(hunger, c5, c1, :ayn, ctrl, 5, 0)
+    wait(5, [c1, c2, c3, c4, c5], parent)
   end
 
-  def wait(0, chopsticks) do
+  def wait(0, chopsticks, parent) do
     Enum.each(chopsticks, fn(c) -> Chopstick.quit(c) end)
+    send(parent, :done)
   end
-  def wait(n, chopsticks) do
+
+  def wait(n, chopsticks, parent) do
     receive do
-      :done ->
-        wait(n - 1, chopsticks)
-      :abort ->
-        Process.exit(self(), :kill)
+      :done -> wait(n - 1, chopsticks, parent)
+      :abort -> Process.exit(self(), :kill)
     end
   end
 end
--- Assignment - Philosophers/philosopher.ex
+++ Assignment - Philosophers/philosopher.ex
@@ -5,67 +5,74 @@ defmodule Philosopher do
     :timer.sleep(:rand.uniform(t))
   end
 
-  def start(hunger, right_chopstick, left_chopstick, name, ctrl, strength) do
-    philosopher = spawn_link(fn -> think(hunger, right_chopstick, left_chopstick, name, ctrl, strength) end)
+  def start(hunger, right_chopstick, left_chopstick, name, ctrl, strength, backoff) do
+    philosopher = spawn_link(fn -> think(hunger, right_chopstick, left_chopstick, name, ctrl, strength, backoff) end)
   end
 
 
-  def eat(0, right_chopstick, left_chopstick, name, ctrl, strength) do
+  def eat(0, right_chopstick, left_chopstick, name, ctrl, strength, backoff) do
     IO.puts "#{name} is done eating"
     send(ctrl, :done)
   end
-  def eat(hunger, right_chopstick, left_chopstick, name, ctrl, strength) do
+  def eat(hunger, right_chopstick, left_chopstick, name, ctrl, strength, backoff) do
     #IO.puts "#{name} is eating"
-    sleep(1000)
+    sleep(100)
 
     Chopstick.return(right_chopstick)
     #IO.puts "#{name} has returned the right chopstick"
     Chopstick.return(left_chopstick)
     #IO.puts "#{name} has returned the left chopstick"
 
-    think(hunger-1, right_chopstick, left_chopstick, name, ctrl, strength)
+    think(hunger-1, right_chopstick, left_chopstick, name, ctrl, strength, backoff)
   end
 
   # Philosophers think/dream for a while before they start eat
-  def think(0, right_chopstick, left_chopstick, name, ctrl, strength) do
-    eat(0, right_chopstick, left_chopstick, name, ctrl, strength)
+  def think(0, right_chopstick, left_chopstick, name, ctrl, strength, backoff) do
+    eat(0, right_chopstick, left_chopstick, name, ctrl, strength, backoff)
   end
-  def think(hunger, right_chopstick, left_chopstick, name, ctrl, strength) do
+  def think(hunger, right_chopstick, left_chopstick, name, ctrl, strength, backoff) do
     #IO.puts "#{name} is thinking"
-    sleep(1000)
-    waiting(hunger, right_chopstick, left_chopstick, name, ctrl, strength)
+    sleep(100)
+    waiting(hunger, right_chopstick, left_chopstick, name, ctrl, strength, backoff)
   end
 
 
-  def waiting(hunger, right_chopstick, left_chopstick, name, ctrl, 0) do
+  def waiting(hunger, right_chopstick, left_chopstick, name, ctrl, 0, backoff) do
     IO.puts "#{name} died."
     send(ctrl, :done)
   end
-  def waiting(hunger, right_chopstick, left_chopstick, name, ctrl, strength) do
-    Chopstick.request(right_chopstick)
-    Chopstick.request(left_chopstick)
+  def waiting(hunger, right_chopstick, left_chopstick, name, ctrl, strength, backoff) do
+    # Randomly decide which chopstick to pick up first
+    first_chopstick = if :random.uniform(2) == 1, do: right_chopstick, else: left_chopstick
+    second_chopstick = if first_chopstick == right_chopstick, do: left_chopstick, else: right_chopstick
 
-    state1 = Chopstick.granted(right_chopstick)
-    state2 = Chopstick.granted(left_chopstick)
+    Chopstick.request(first_chopstick)
+    Chopstick.request(second_chopstick)
+
+    state1 = Chopstick.granted(first_chopstick)
+    state2 = Chopstick.granted(second_chopstick)
 
     state = {state1, state2}
     #IO.inspect(state)
 
     case state do
       {:no, :no} ->
-        waiting(hunger, right_chopstick, left_chopstick, name, ctrl, strength-1)
+        sleep(backoff)
+        waiting(hunger, right_chopstick, left_chopstick, name, ctrl, strength-1, backoff+100)
         :no
       {:no, :ok} ->
-        Chopstick.return(left_chopstick)
-        waiting(hunger, right_chopstick, left_chopstick, name, ctrl, strength-1)
+        Chopstick.return(second_chopstick)
+        sleep(backoff)
+        waiting(hunger, right_chopstick, left_chopstick, name, ctrl, strength-1, backoff+100)
         :no
       {:ok, :no} ->
-        Chopstick.return(right_chopstick)
-        waiting(hunger, right_chopstick, left_chopstick, name, ctrl, strength-1)
+        Chopstick.return(first_chopstick)
+        sleep(backoff)
+        waiting(hunger, right_chopstick, left_chopstick, name, ctrl, strength-1, backoff+100)
         :no
       {:ok, :ok} ->
         #IO.puts "#{name} has both chopsticks"
-        eat(hunger, right_chopstick, left_chopstick, name, ctrl, strength)
+        eat(hunger, right_chopstick, left_chopstick, name, ctrl, strength, 0)
     end
   end
 
